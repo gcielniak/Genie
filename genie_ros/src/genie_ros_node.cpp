04c565a3b07931a5504f32da358ad17842b57476
Grzegorz Cielniak <gcielniak@lincoln.ac.uk>
2017-10-13 16:59:34 +0100

    ros_node: all four images published

--- genie_ros/src/genie_ros_node.cpp
+++ genie_ros/src/genie_ros_node.cpp
@@ -8,20 +8,23 @@
 
 int main(int argc, char **argv) {
 
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "genie");
 
-  ros::NodeHandle n;
-  ros::Publisher image_pub = n.advertise<sensor_msgs::Image>("camera/image", 100);
-
-  ros::Rate loop_rate(1);
-
-  int count = 0;
-
-  sensor_msgs::Image msg;
-
+  ros::NodeHandle nh;
+  ros::Publisher rgb_pub = nh.advertise<sensor_msgs::Image>("camera/rgb_image", 100);
+  ros::Publisher nir_pub = nh.advertise<sensor_msgs::Image>("camera/nir_image", 100);
+  ros::Publisher rgbn_pub = nh.advertise<sensor_msgs::Image>("camera/rgbn_image", 100);
+  ros::Publisher ndvi_pub = nh.advertise<sensor_msgs::Image>("camera/ndvi_image", 100);
+	
+  int rgb_count = 0;
+  int nir_count = 0;
+  int rgbn_count = 0;
+  int ndvi_count = 0;
+	
+  sensor_msgs::Image image_msg;
 	cv_bridge::CvImage img_bridge;
 
-std_msgs::Header header; // empty header
+	std_msgs::Header header; // empty header
 
 	GenICam::JAI::AD_130GE camera;
 
@@ -30,24 +33,60 @@ std_msgs::Header header; // empty header
 		camera.Start();
 	}
 	catch (std::exception& e) {
-		cerr << e.what() << endl;
+		std::cerr << e.what() << std::endl;
+		return 0;
 	}
 
-	cerr << "Camera Initialised." << endl;
+	cerr << "RGBN Camera Initialised." << endl;
 
   while (ros::ok()) {
 
-	if (image_pub.getNumSubscribers()) {
-		camera.Capture();
-		header.seq = count; // user defined counter
-		header.stamp = ros::Time::now(); // time
-		img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::MONO8, camera.NDVIImage());
-		img_bridge.toImageMsg(msg); // from cv_bridge to sensor_msgs::Image
-	    image_pub.publish(msg);
-	    ++count;
-	}
+		if (rgb_pub.getNumSubscribers() ||
+		nir_pub.getNumSubscribers() ||
+		rgbn_pub.getNumSubscribers() ||
+		ndvi_pub.getNumSubscribers()) {
+			camera.Capture();
+			
+			header.stamp = ros::Time::now(); // time
 
-    ros::spinOnce();
+			//rgb images
+			if (rgb_pub.getNumSubscribers()) {
+				header.seq = rgb_count++;
+				img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::BGR8,
+					camera.RGBImage());
+				img_bridge.toImageMsg(image_msg);
+				rgb_pub.publish(image_msg);
+			}
+
+			//nir images
+			if (nir_pub.getNumSubscribers()) {
+				header.seq = nir_count++;
+				img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::MONO8,
+					camera.NIRImage());
+				img_bridge.toImageMsg(image_msg);
+				nir_pub.publish(image_msg);
+			}
+
+			//rgbn images
+			if (rgbn_pub.getNumSubscribers()) {
+				header.seq = rgbn_count++;
+				img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::BGRA8,
+					camera.Image());
+				img_bridge.toImageMsg(image_msg);
+				rgbn_pub.publish(image_msg);
+			}
+
+			//ndvi images
+			if (ndvi_pub.getNumSubscribers()) {
+				header.seq = ndvi_count++;
+				img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::MONO8,
+					camera.NDVIImage());
+				img_bridge.toImageMsg(image_msg);
+				ndvi_pub.publish(image_msg);
+			}
+		}
+
+		ros::spinOnce();
   }
 
   return 0;
