90f603b3d37b769f05630a0658d34f4d338a6f07
gcielniak <gcielniak@gmail.com>
2017-10-23 16:46:15 +0100

    4 panel visualisation (not tested)

--- Apps/src/GenieApp.cpp
+++ Apps/src/GenieApp.cpp
@@ -18,7 +18,23 @@ int main(int argc, char* argv[]) {
 
 			camera.Capture();
 
-			cv::Mat image = camera.NDVIImage();
+			cv::Mat rgb_image = camera.RGBImage();
+
+			cv::Mat vis_image = rgb_image;
+			std::vector<cv::Mat> channels;
+			cv::split(rgb_image, channels); // break image into channels
+			cv::merge({ { channels[2], channels[2], channels[2] } }, vis_image);
+
+			cv::Mat nir_image = camera.NIRImage();
+			cv::merge({ { nir_image, nir_image, nir_image} }, nir_image);
+
+			cv::Mat ndvi_image = camera.NDVIImage();
+			cv::merge({ { ndvi_image, ndvi_image, ndvi_image } }, ndvi_image);
+
+			cv::Mat row_1, row_2, image;
+			cv::hconcat(row_1, vis_image, nir_image);
+			cv::hconcat(row_2, rgb_image, ndvi_image);
+			cv::vconcat(image, row_1, row_2);
 
 			cv::imshow(window_name, image);
 
